df2702df513b540f45867c644509b94ea35dd390
yerdboy <ggg28828282gv@gmail.com>
2024-04-23 09:20:37 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+final List<String> users = ["Tom", "Bob", "Sam", "Mike"];
+final List<String> companies = ["Google", "Microsoft", "Apple", "JetBrains"];
+
+void main() {
+  runApp(MyApp()); // Remove the parentheses here
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("MyApp"),
+        ),
+        body: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: users.length,
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(
+                users[index],
+                style: const TextStyle(fontSize: 22),
+              ),
+              leading: const Icon(Icons.face),
+              trailing: const Icon(Icons.phone),
+              subtitle: Text("Works in ${companies[index]}"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
